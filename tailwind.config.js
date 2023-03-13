166aa02a680d2480ffcbd0a824635e4cb0c01c00
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2023-03-13 11:03:35 +0200

    Tailwind Template 1.0

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // Path to source code
+    content: ["./index.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
